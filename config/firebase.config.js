5d9db3f507a28ec7ad89ba341482d068f23cd63b
tinatripak <61850907+tinatripak@users.noreply.github.com>
2024-01-11 01:53:49 -0500

    Update firebase.config.js

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccout");
+var serviceAccount = require("./serviceAccout.js");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)

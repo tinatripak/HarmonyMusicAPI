8cf6f5afa6163235eaf09acc5e1293bc6921f0bf
Kristina Tripak <tinatripak2002@gmail.com>
2024-01-11 20:19:29 -0500

    fixed

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccount.js");
+var serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
